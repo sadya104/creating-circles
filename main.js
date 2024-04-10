df346d2bd563ef5eb2d789778e74f38d78dd538c
sadya104 <sandrickson@pursuit.com>
2024-04-10 16:52:08 -0400

    add link

--- main.js
+++ main.js
@@ -21,6 +21,8 @@ button.addEventListener('click', function (){
     count++
 })
 
+
+//function to generate colors
 function generateRandomColor(){
      return '#' + Math.floor(Math.random()*16777215).toString(16);
 }
